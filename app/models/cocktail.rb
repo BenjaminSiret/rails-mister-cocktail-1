6884f99d64e30c31847add210100b72df2918268
morgane duluc <morgane.duluc@gmail.com>
2021-03-04 16:52:46 +0100

    Mister Cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,8 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  # belongs_to :description
+  # has_and_belongs_to_many :ingredients
+
   validates :name, presence: true, uniqueness: true
 end
